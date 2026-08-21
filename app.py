95224a247fd1b5ba08b93842a9ad6b40072be21c
Artur <safuanovartur@mail.ru>
2026-08-21 16:18:10 +0300

    Доделал дашбордик

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import plotly.graph_objects as go
 
 st.set_page_config(
     page_title='Дашборд по инцидентам',
@@ -27,8 +28,10 @@ logs_with_emp = logs.merge(employees, how='left', left_on='user_id', right_on='i
 ## Настраиваем sidebar
 st.sidebar.title('Сайдбар')
 
-selected_departments = st.sidebar.multiselect('Выберите отдел', employees['department'].unique(), default=employees['department'].unique())
-selected_access = st.sidebar.multiselect('Уровень доступа', employees['access_logs'].unique(), default=employees['access_logs'].unique())
+selected_departments = st.sidebar.multiselect('Выберите отдел', employees['department'].unique(),
+                                              default=employees['department'].unique())
+selected_access = st.sidebar.multiselect('Уровень доступа', employees['access_logs'].unique(),
+                                         default=employees['access_logs'].unique())
 start_date = pd.to_datetime(st.sidebar.date_input('Дата начала', value=logs['date'].min()))
 end_date = pd.to_datetime(st.sidebar.date_input('Дата конца', value=logs['date'].max()))
 
@@ -38,7 +41,7 @@ filtered_logs = logs_with_emp[
     (logs_with_emp['access_logs'].isin(selected_access)) &
     (logs_with_emp['date'] >= start_date) &
     (logs_with_emp['date'] <= end_date)
-]
+    ]
 
 filtered_employees = employees[employees['department'].isin(selected_departments)]
 
@@ -51,7 +54,6 @@ col1, col2, col3, col4 = st.columns(4, gap='small')
 all_logs = len(filtered_logs)
 failure_logs = len(filtered_logs[filtered_logs['status'] == 'failure'])
 
-
 col1.metric('Всего логов', all_logs)
 col2.metric('Неудачные входы', failure_logs)
 col3.metric('Процент неудачных входов', round(failure_logs / all_logs * 100))
@@ -59,14 +61,16 @@ col4.metric('Количество уникальных сотрудников', 
 
 ## Графики
 col5, col6 = st.columns(2, gap='medium')
-fig1 = px.bar(filtered_employees, x='department', color='department', title='Количество сотрудников в зависимости от отдела')
+fig1 = px.bar(filtered_employees, x='department', color='department',
+              title='Количество сотрудников в зависимости от отдела')
 fig1.update_layout(xaxis={'categoryorder': 'total descending'})
 col5.plotly_chart(fig1, width='stretch')
 
 fig2 = px.pie(filtered_logs, names='status', title='Доля успешных/неудачных входов')
 col6.plotly_chart(fig2, width='stretch')
 
-groupby_failure = filtered_logs[filtered_logs['status'] == 'failure'].groupby(pd.Grouper(key='date', freq='W'))['status'].count()
+groupby_failure = filtered_logs[filtered_logs['status'] == 'failure'].groupby(pd.Grouper(key='date', freq='W'))[
+    'status'].count()
 fig3 = px.line(groupby_failure, title='Динамика неудачных входов по неделям')
 st.plotly_chart(fig3, width='stretch')
 
@@ -82,3 +86,17 @@ fig5 = px.bar(top_groupby_emp_failure, x='status', y='name', color='name',
               title='Топ человек по неудачным входам в систему', orientation='h')
 fig5.update_layout(yaxis={'categoryorder': 'total ascending'})
 col8.plotly_chart(fig5, width='stretch')
+
+admin_with_failure = filtered_logs[(filtered_logs['status'] == 'failure') & (filtered_logs['access_logs'] == 'admin')]
+admin_with_failure_clean = admin_with_failure.groupby(['user_id', 'name', 'department'])['status'].count().reset_index()
+table = admin_with_failure_clean.query('status > 5').rename({'status': 'attempts'}, axis=1)
+st.dataframe(data=table, width='stretch', height='stretch')
+
+## Выводы
+groupby_dep_failure = filtered_logs[filtered_logs['status'] == 'failure'].groupby(['department'])['status'].count()
+top = groupby_dep_failure.sort_values(ascending=False).head().reset_index().loc[0, 'department']
+st.write(f'Отдел, лидирующий по неудачным входам: {top}')
+
+st.write(f'Количество администраторов, превысивших порог в 5 неудач: {len(table)}')
+
+st.write(f'Количество удачных входов: {all_logs - failure_logs}')
